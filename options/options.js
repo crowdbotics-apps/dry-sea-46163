3b85face2995d129e381d22bb65ed5e815a65a44
Crowdbotics <team@crowdbotics.com>
2024-02-08 20:54:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-sea-46163",
+  url: "https://dry-sea-46163.botics.co",
+  api: "https://dry-sea-46163.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
